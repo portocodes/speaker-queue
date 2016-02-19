e85f8b3e786e60d351c633a1a725ddf601824e26
Ricardo Mendes <rokusu@gmail.com>
2016-02-19 20:14:18 +0000

    Adiciona mais campos ao scaffolding

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -69,6 +69,6 @@ class TalksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def talk_params
-      params.require(:talk).permit(:title, :description)
+      params.require(:talk).permit(:title, :description, :speaker, :duration)
     end
 end
