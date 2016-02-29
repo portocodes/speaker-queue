341a166d9e380e124448c96a883a340bd017674a
ricardojusto <ricardojusto@outlook.com>
2016-02-29 23:16:40 +0000

    add CRUD for talks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
-  root 'talks#new'
+  root 'talks#index'
 
-  resources :talks, except: [:destroy, :edit]
+  resources :talks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
