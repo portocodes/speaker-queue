1a1b3c794e63667f8a301f686fdfe618fe409937
ricardojusto <ricardojusto@outlook.com>
2016-03-03 18:07:49 +0000

    set template for new talk created mailer

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,12 +1,13 @@
 class TalkMailer < ActionMailer::Base
 
   def talk_created(user)
+    @user = user
+    #last talk created is the new talk for the current user
+    @talk = @user.talks.last
     mail(
       to: user.email,
-      from: "someone@gmail.com", #using GMAIL this line isnt read. with other providers this is considered,
-      subject: "New Talk Created",
-      body: "This is the body from my first mailer"
+      from: "someone@somedomain.com", #if using GMAIL this line isn't read. with other providers this is considered.
+      subject: "New Talk Created"
     )
   end
-
 end
