3a34fb7be6b5705dd985122dc5b0de496935e170
ricardojusto <ricardojusto@outlook.com>
2016-03-03 20:34:33 +0000

    add mail uniqueness validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email, uniqueness: true
   has_many :talks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
