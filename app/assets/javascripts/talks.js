49668b66a0556747897121ab11b19483f070cd64
ricardojusto <ricardojusto@outlook.com>
2016-04-18 15:43:49 +0100

    update gemfile - remove twitter bootstrap and other gems

--- app/assets/javascripts/talks.js
+++ app/assets/javascripts/talks.js
@@ -1,7 +1,6 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
     $( "#datepicker" ).datepicker({
       dateFormat: "dd-MM-yy"
     });
