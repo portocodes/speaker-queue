d57c1acc22e74492c33b374d1456de40514a0250
ricardojusto <ricardojusto@outlook.com>
2016-04-26 14:16:54 +0100

    add a main.js file for global functions

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,14 @@
+// Enable Title page to change when it's not active
+var pageTitle = document.title;
+document.addEventListener("visibilitychange", function(event){
+    if ( document.hidden )
+        document.title = "Miss You :(";
+    else
+        document.title = pageTitle;
+ });
+
+//Tooltip
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  console.log(1);
+});
