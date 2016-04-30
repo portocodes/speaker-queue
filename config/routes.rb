78299fa0bcd87c4df897142f1ee3b27cb0932fd3
ricardojusto <ricardojusto@outlook.com>
2016-04-30 03:37:25 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#main'
+  get  'about_us', to: 'pages#about_us',   as: 'about_us'
 
-  get 'about_us',  to: 'pages#about_us', as: "about_us"
-
-  get 'submit',    to: 'users#submit',     as: 'submit'
+  get  'submit',   to: 'users#submit',     as: 'submit'
   post 'submit',   to: 'users#create'
 
-  get 'signup',    to: 'users#new',        as: 'signup'
-  get 'login' ,    to: 'sessions#new',     as: 'login'
-  get 'logout',    to: 'sessions#destroy', as: 'logout'
+  get  'signup',   to: 'users#new',        as: 'signup'
+  get  'login' ,   to: 'sessions#new',     as: 'login'
+  get  'logout',   to: 'sessions#destroy', as: 'logout'
 
   get 'tags/:tag', to: 'talks#index',      as: :tag
 
