a22dd33ef56827f7117c5966ae69dca0bff45be0
Hugo Peixoto <hugo.peixoto@gmail.com>
2017-03-17 23:19:47 +0000

    Remove code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,19 @@
 class UsersController < ApplicationController
-  load_and_authorize_resource
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @users = User.all unless current_user
-  end
-
-  def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def submit
-    @user ||= User.new
-    @talk = @user.talks.build
-  end
-
-
   def edit
-  end
-
-  def create
-    @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id unless current_user
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = current_user
+    @talks = @user.talks.where(state: 'pending')
   end
 
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+      if current_user.update(user_params)
+        format.html {
+          if params[:from] == 'new-talk'
+            redirect_to new_talk_path, notice: 'Preferences updated.'
+          else
+            redirect_to root_path, notice: 'Preferences updated.'
+          end
+        }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -48,41 +22,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email,:role, :avatar, :remote_avatar_url, :remove_avatar,
-                                   talks_attributes: [
-                                    :user_id,
-                                    :id,
-                                    :title,
-                                    :description,
-                                    :duration,
-                                    :month,
-                                    :speaker,
-                                    :state,
-                                    :tag_list,
-                                    :video,
-                                    :resource,
-                                    :time_event,
-                                    :talk_date,
-                                    :talk_time,
-                                    :location_name,
-                                    :location_coordinates
-                                    ]
-                                  )
-    end
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
